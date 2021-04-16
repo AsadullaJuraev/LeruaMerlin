0df5f1a00b3bcff785d6d887c3a98c9ccb6895da
Asadulla Juraev <tezkormedia@gmail.com>
2021-04-16 12:00:11 +0300

    Initial Commit

---
+++ LeruaMerlin/viewModel/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  LeruaMerlin
+//
+//  Created by Asadulla Juraev on 16.04.2021.
+//
+
+import SwiftUI
+
+class ViewModel: ObservableObject{
+    @Published var offset: CGFloat = 0
+}
